89528e57b302a8f60966a1fa7892f11c5ea49ed0
Ewan <ewan15@users.noreply.github.com>
2021-01-26 19:38:53 +0000

    Update main.py

--- main.py
+++ main.py
@@ -27,6 +27,21 @@ def loada2(state):
     global connected
     connected = state
 
+@client.event
+async def on_message(message):
+    if message.content != '-its over anakin':
+        return
+
+    try:
+        voice_channel = message.author.voice.channel
+        print(f"Joining: {voice_channel}")
+        voice = await voice_channel.connect()
+        voice.play(discord.FFmpegPCMAudio('obi-wan-hello-there.mp3'))
+        await asyncio.sleep(2.5)
+        await voice.disconnect()
+    except Exception as e:
+        print(e)
+
 
 @client.event
 async def on_voice_state_update(member, prev, cur):
